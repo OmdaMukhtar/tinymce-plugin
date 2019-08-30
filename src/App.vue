26c6c9873dfc2d35195f0fa73d42d95320f58d87
omda <abuoop123@gmail.com>
2019-08-30 17:27:02 +0200

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <editor-app  />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import editorApp from './components/editor/index'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    editorApp
   }
 }
 </script>
---
+++ src/components/editor/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <h2 class="">How can We Add Plugins to Tinymce-Vue</h2>
+        <editor id="app-editor" api-key="jh5j9j89wyv5mbctvucc67e0o5ba3q7v4xkldjfoaqv74glt" :init="inital" />
+    </div>
+</template>
+
+<script>
+import editor from '@tinymce/tinymce-vue';
+export default {
+    components: {
+        editor
+    },
+    computed: {
+        inital(){
+            return {plugins: 'wordcount'};
+        }
+    }
+}
+</script>
+
+<style  scoped>
+    #app-editor{
+        width: 50%;
+    }
+
+    .container{
+        width: 720px;
+        margin:100px auto;
+    }
+</style>
